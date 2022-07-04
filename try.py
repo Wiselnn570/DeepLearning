1c67c9b19f10b166268e89adc4d64b1aeb76f8a2
Wiselnn <Wiselnn570@Gmail.com>
2022-07-04 21:42:06 +0800

    implementation of softmax regression

--- try.py
+++ try.py
@@ -1,8 +1,9 @@
 from math import ceil, floor
 import numpy as np
 from matplotlib import pyplot as plt
+import torch
 
-def Print(name = "wxl"):
+def Print(name: str =  "wxl"):
     print("my name is %s" % (name))
 
 
@@ -15,8 +16,16 @@ class wxl:
 def Try(a, b, *, name):
     print("! {}".format(name))
 
+def Test(a, b):
+    assert a > 1, a
+    assert b < 0, b
+
+def ret():
+    return 1, 2
+
 
 if __name__ == "__main__":
+    # Print()
     # my_name = wxl("Wiselnn")
     # my_name()
     # your_name = Print
@@ -32,7 +41,16 @@ if __name__ == "__main__":
     # axs[0, 1].set_title('Sharing Y axis')
     # axs[1, 1].scatter(x, y)
     # plt.show()
-    x = [1, 2, 3, 4, 5]
-    y = [2, 4, 6, 8, 10]
-    for i, (a, b) in enumerate(zip(x, y)):
-        print("position %d, y / x = %d" % (i, b / a))
+    # x = [1, 2, 3, 4, 5]
+    # y = [2, 4, 6, 8, 10]
+    # for i, (a, b) in enumerate(zip(x, y)):
+    #     print("position %d, y / x = %d" % (i, b / a))
+    # X = torch.zeros(10, requires_grad=True)
+    # with torch.no_grad():
+    #     y = X * 2
+    #     print(y.requires_grad)
+    A = [1, 2, 3]
+    B = [2, 3, 4]
+    C = [3, 4, 5, 6]
+    for a, b, c in zip(A, B, C):
+        print(a, b, c)
